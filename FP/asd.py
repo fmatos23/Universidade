95d0340c1fa8c82a97dca0ce34c30cb2a9309802
fmatos23 <franciscojosematos23@ua.pt>
2024-06-11 22:11:46 +0100

    updates

--- FP/asd.py
+++
@@ -1,4 +0,0 @@
-def reverse(s):
-    return s if len(s) <= 1 else s[-1:] + reverse(s[:-1])
-
-print(reverse("hello"))
--- FP/bise.py
+++
@@ -1,8 +0,0 @@
-import bisect
-
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(bisect.bisect(lista, 5))  # 5
-
-print(bisect.bisect(lista, 10))  # 9
-
-print(bisect.bisect_right(lista, 5))  # 5
